00701904d100085710b0fcecf0e033dd3dd28b7b
keshav <keshavyogi1234@gmail.com>
2026-02-03 09:57:41 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,15 @@
+let score = 33
+
+console.log(typeof score)
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 = true; 0 = false
+// "" = flase
+// "keshav" = true
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
